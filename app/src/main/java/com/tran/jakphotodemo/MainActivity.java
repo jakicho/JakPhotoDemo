f0b64be9d2a4997ea88edda1c9dffc3d1c40b544
jako <tranjak@yahoo.fr>
2016-12-21 12:04:12 +0100

    Adding MainFragment, RecyclerView, Butterknife

--- app/src/main/java/com/tran/jakphotodemo/MainActivity.java
+++ app/src/main/java/com/tran/jakphotodemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tran.jakphotodemo;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setMainFragment(savedInstanceState);
+    }
+
+    // adding a layer of modularity rather than puting everything on the MainActivity
+    private void setMainFragment(Bundle savedInstanceState) {
+
+        // if savedInstanceState exists, we know that the fragment already exist
+        if (savedInstanceState == null) {
+            MainFragment mFrag = new MainFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.activity_main, mFrag, "main_frag").commit();
+        }
     }
 }
---
+++ app/src/main/java/com/tran/jakphotodemo/MainFragment.java
@@ -0,0 +1,32 @@
+package com.tran.jakphotodemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class MainFragment extends Fragment {
+
+    @Bind(R.id.recycler_view) public RecyclerView recyclerView;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+}
